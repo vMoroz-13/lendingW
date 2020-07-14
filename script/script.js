29656de8ce39d040c9aed75eec2f0c816b5752f9
Vadim Moroz <vmoroz13@yandex.ru>
2020-07-14 15:58:03 +0300

    less26

--- script/script.js
+++ script/script.js
@@ -221,7 +221,7 @@ window.addEventListener('DOMContentLoaded', () => {
         startSlide(1500);
     };
     slider();
-//Замена фото
+    //Замена фото
     const showPhoto = () => {
         const commandPhoto = document.querySelectorAll('.command__photo');
 
@@ -236,7 +236,7 @@ window.addEventListener('DOMContentLoaded', () => {
 
     };
     showPhoto();
-//Запрет на ввод букв
+    //Запрет на ввод букв
     document.oninput = () => {
         const calcSquare = document.querySelector('.calc-square'),
             calcCount = document.querySelector('.calc-count'),
@@ -245,8 +245,61 @@ window.addEventListener('DOMContentLoaded', () => {
         calcCount.value = calcCount.value.replace(/[^\+\d]/g, '');
         calcDay.value = calcDay.value.replace(/[^\+\d]/g, '');
     };
+    //Калькулятор
+
+    const calc = (price = 100) => {
+
+        const calcBlock = document.querySelector('.calc-block'),
+            calcType = document.querySelector('.calc-type'),
+            calcSquare = document.querySelector('.calc-square'),
+            calcCount = document.querySelector('.calc-count'),
+            calcDay = document.querySelector('.calc-day'),
+            totalValue = document.getElementById('total');
+
+        const countSum = () => {
+            let total = 0,
+                countValue = 1,
+                dayValue = 1;
+            const typeValue = calcType.options[calcType.selectedIndex].value;
+            const cquareValue = +calcSquare.value;
+
+            if (calcCount.value > 1) {
+                countValue += (calcCount.value - 1) / 10;
+            }
+
+            if (calcDay.value && calcDay.value < 5) {
+                dayValue *= 2;
+            } else if (calcDay.value && calcDay.value < 10) {
+                dayValue *= 1.5;
+            }
+
+            if (typeValue && cquareValue) {
+                total = price * typeValue * cquareValue * countValue * dayValue;
+            }
+            totalValue.textContent = total;
+        };
+
+        calcBlock.addEventListener('change', e => {
+            const target = e.target;
+            if (target === calcType || target === calcSquare ||
+           target === calcCount || target === calcDay) {
+                countSum();
+            }
+        });
+    };
+    calc(100);
+    //send-ajax-form
+    const sendform = () => {
+        const errorMessage = 'Что то пошло не так....',
+            loadMesage = 'Идет загрузка... ',
+            successMessage = 'Спасибо мы скоро с Вами свяжемся';
+
+        const form = document.getElementById('form1');
+        const statusMassege = document.createElement('div');
+        statusMassege.textContent = 'Сдесь будет сообщение';
+        statusMassege.style.cssText = 'font-size: 2rem;';
+        form.appendChild(statusMassege);
+    };
+    sendform();
 
 });
-
-
-
